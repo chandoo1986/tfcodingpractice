88abb2bf793e938d26b00d0c16091b68b256aedd
chandoo1986@gmail.com <chandoo1986@gmail.com>
2025-05-27 15:43:25 -0500

    Updated bucket names

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "example" {
   count  = 5
-  bucket = "chandra-bucket-${count.index}"
+  bucket = "chandra-bucket-05272025-${count.index}"
 
   tags = {
     Name = "dev-${count.index}"
