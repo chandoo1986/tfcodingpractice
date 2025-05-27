e5aa4cf145a0aeebe3886f82908cffac4a5b391e
chandoo1986@gmail.com <chandoo1986@gmail.com>
2025-05-27 15:49:18 -0500

    Added iam users block with dynamic names

--- main.tf
+++ main.tf
@@ -1,8 +1,16 @@
-resource "aws_s3_bucket" "example" {
+//Basic count usage ofr bucket creation
+/*resource "aws_s3_bucket" "example" {
   count  = 5
   bucket = "chandra-bucket-05272025-${count.index}"
 
   tags = {
     Name = "dev-${count.index}"
   }
+}
+*/
+
+//Create IAM users with dynamic names
+resource "aws_iam_user" "dev_users" {
+  count = 3
+  name  = "dev-user-${count.index}"
 }
