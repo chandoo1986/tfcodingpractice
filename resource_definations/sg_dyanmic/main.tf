554f913043592928b427a79b6a1755ab1ec4c233
chandoo1986@gmail.com <chandoo1986@gmail.com>
2025-06-10 11:21:20 -0500

    -h

--- resource_definations/sg_dyanmic/main.tf
+++ resource_definations/sg_dyanmic/main.tf
@@ -1,4 +1,27 @@
-resource "aws_subnet" "myvpcsubnets" {
-  cidr_block = "10.0.0.0/20"
-  vpc_id    = "10.0.0.0/16"
+resource "aws_security_group" "sg-webserver" {
+  // vpc_id      = aws_vpc.vpc.id
+  vpc_id      = "10.0.0.0/16"
+  name        = "webserver"
+  description = "Security Group for Web Servers"
+
+  dynamic "ingress" {
+    for_each = local.inbound_rules
+    content {
+      from_port   = ingress.value.port
+      to_port     = ingress.value.port
+      protocol    = "tcp"
+      cidr_blocks = [ingress.value.cidr_block]
+    }
+  }
+
+  dynamic "egress" {
+    for_each = local.outbound_ports
+    content {
+      from_port   = egress.value
+      to_port     = egress.value
+      protocol    = "tcp"
+      cidr_blocks = ["10.0.0.0/16"]
+    }
+  }
 }
+
